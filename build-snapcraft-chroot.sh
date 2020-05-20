0788e4b8a1fdd1bcc98063a9feb3ba28e4fd1b5f
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2020-05-20 16:47:01 +0200

    Add script to build chroot for snapcraft

---
+++ build-snapcraft-chroot.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -ex
+# To install the script requirements:
+# apt install qemu qemu-user-static binfmt-support debootstrap
+
+if [ "$(id -u)" -ne 0 ]; then
+    printf "You need to run the script as root user\n"
+    exit 1
+fi
+
+arch=armhf
+series=focal
+chroot_d=$arch-$series-rootfs
+# coreXX must match series (18 if bionic, 20 if focal, etc.)
+snaps=(snapcraft core20)
+
+# Create chroot, this can take a while...
+qemu-debootstrap --arch $arch $series $chroot_d
+
+# Download snaps that we will "install" in the chroot
+for snap in "${snaps[@]}"; do
+    UBUNTU_STORE_ARCH=$arch snap download --basename="$snap" "$snap"
+    mkdir -p "$chroot_d/snap/$snap"
+    unsquashfs -d "$chroot_d/snap/$snap/current" "$snap".snap
+    rm "$snap".snap "$snap".assert
+done
+
+# Let snapcraft think it is in a docker container
+touch $chroot_d/.dockerenv
+
+# Script to invoke snapcraft in the chroot
+cat > $chroot_d/usr/bin/snapcraft << EOF
+#!/bin/sh -e
+export LANG="en_US.UTF-8" LANGUAGE="en_US:en" LC_ALL="en_US.UTF-8" PATH="/snap/bin:\$PATH" SNAP="/snap/snapcraft/current" SNAP_NAME="snapcraft" SNAP_ARCH="$arch"
+export SNAP_VERSION=\$(awk '/^version:/{print \$2}' /snap/snapcraft/current/meta/snap.yaml)
+exec "/snap/snapcraft/current/usr/bin/python3" "/snap/snapcraft/current/bin/snapcraft" "\$@"
+EOF
+
+chmod +x $chroot_d/usr/bin/snapcraft
+
+# Get proper locale
+chroot $chroot_d locale-gen en_US.UTF-8
+DEBIAN_FRONTEND=noninteractive chroot $chroot_d dpkg-reconfigure locales
